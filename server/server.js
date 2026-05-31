119d90dc45b3f835ce460bf27c96231759ea6e6f
Kunal72004 <143131261+Kunal72004@users.noreply.github.com>
2026-05-31 17:37:53 +0530

    fix cors for localhost

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://education-point-ten.vercel.app",
+    origin: ["https://education-point-ten.vercel.app","http://localhost:3000"],
     credentials: true,
   }),
 );
